2221522c58fb5ce0857fe31df7ed4477a09ec66e
tonisann <haraanton.s@gmail.com>
2023-01-16 04:48:04 +0200

    16.01.2023 ranok

--- kata_arr_train.py
+++ kata_arr_train.py
@@ -1,3 +1,73 @@
+def xo(s):
+    counterX = 0
+    counterO = 0
+    arrchx = list()
+    arrcho = list()
+    # count = 0
+    temps = s.lower()
+    tempsmn = set()
+    for temp1 in temps:
+        tempsmn.add(temp1)
+
+    print(tempsmn)
+
+    countearru = 0
+    arrU = list()
+    for temphu in tempsmn:
+        arrU.append(temphu)
+        countearru += 1
+        print(f"Write: {arrU}")
+
+    # tempsmn.add(temps)
+    # print(tempsmn)
+    for temp in temps:
+        if temp == 'x':
+            arrchx.append(temp)
+            print(arrchx)
+            counterX += 1
+        else:
+            temp == 'o'
+            arrcho.append(temp)
+            print(arrcho)
+            counterO += 1
+
+    if len(arrchx) == len(arrcho):
+        return True
+    else:
+        return False
+
+
+print(xo('XoxxooOOsdffwe'))
+
+"""def xo(s):
+    temp_har = s
+    count_temp_x = 0
+    count_temp_o = 0
+    counter = 0
+    for char in temp_har:
+        if char == "x":
+            count_temp_x += 1
+            print(count_temp_x)
+        elif char == "o":
+            count_temp_o += 1
+            print(count_temp_o)
+        else:
+            counter += 1
+            print(counter)
+
+    if count_temp_x == count_temp_o:
+        return True
+    else:
+        return False"""
+
+
+# xo('xo')
+# print("next")
+# xo('xo0')
+# print("next")
+# xo('xxxoo')
+
+
 def check(seq, elem):
     for tepm in seq:
         if tepm == elem:
